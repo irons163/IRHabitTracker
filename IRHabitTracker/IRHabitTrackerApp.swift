6bbf2cc952c2f8ff0674130c16404fbde7498435
Phil <irons7654321@gmail.com>
2025-10-11 21:54:59 +0800

    Initial Commit

---
+++ IRHabitTracker/IRHabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  IRHabitTrackerApp.swift
+//  IRHabitTracker
+//
+//  Created by Phil on 2025/10/11.
+//
+
+import SwiftUI
+
+@main
+struct IRHabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IRHabitTrackerTests/IRHabitTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  IRHabitTrackerTests.swift
+//  IRHabitTrackerTests
+//
+//  Created by Phil on 2025/10/11.
+//
+
+import Testing
+@testable import IRHabitTracker
+
+struct IRHabitTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
